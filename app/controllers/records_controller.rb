d49fa0de0e3755339c3ebf60156088ddbbcf41e2
Rob Kitromilides <robkitro@gmail.com>
2015-08-16 18:20:46 +0100

    Delete buton and link to database

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -15,6 +15,15 @@ class RecordsController < ApplicationController
 	      end
 	    end
 
+	def destroy
+      @record = Record.find(params[:id])
+      @record.destroy
+      head :no_content
+    end
+
+
+
+
 	    private
 
 	      def record_params
